e2c2892bb2da113fb29fcdf0f19eb5e4899aaa6b
KK <919578485@qq.com>
2024-12-31 18:42:25 +0800

    首页修改

--- app/src/main/java/com/clean/filecleaner/ui/module/MainActivity.kt
+++ app/src/main/java/com/clean/filecleaner/ui/module/MainActivity.kt
@@ -5,32 +5,38 @@ import android.os.Bundle
 import com.clean.filecleaner.databinding.ActivityMainBinding
 import com.clean.filecleaner.ext.immersiveMode
 import com.clean.filecleaner.ui.base.StoragePermissionBaseActivity
+import com.clean.filecleaner.ui.module.dialog.CommonDialog
 import com.clean.filecleaner.ui.module.junk.JunkSearchActivity
 
+//        TransitionManager.beginDelayedTransition(binding.root)
 class MainActivity : StoragePermissionBaseActivity<ActivityMainBinding>() {
-    override fun setupImmersiveMode() = immersiveMode()
+    override fun setupImmersiveMode() = immersiveMode(binding.root)
     override fun inflateViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
     override fun initView(savedInstanceState: Bundle?) {
 
-//        TransitionManager.beginDelayedTransition(binding.root)
+        with(binding) {
 
-        binding.request.setOnClickListener {
-            requestPermissions {
-                startActivity(Intent(this@MainActivity, JunkSearchActivity::class.java))
+            btnClean.setOnClickListener {
+                requestPermissions {
+                    startActivity(Intent(this@MainActivity, JunkSearchActivity::class.java))
+                }
+            }
 
+            appManager.setOnClickListener {
+                CommonDialog(message = "dsfsdfsdfsd", leftBtn = "Cancel", rightBtn = "OK", cancelable = true,
+                    leftClick = {
 
-//                CommonDialog(message = "dsfsdfsdfsd", leftBtn = "Cancel", rightBtn = "OK", cancelable = true,
-//                    leftClick = {
-//
-//                    },
-//                    rightClick = {
-//
-//                    }).show(supportFragmentManager, "CommonDialog")
+                    },
+                    rightClick = {
 
+                    }).show(supportFragmentManager, "CommonDialog")
 
             }
+
+
         }
+
 
     }
 }
---
+++ app/src/main/java/com/clean/filecleaner/ui/module/MainBeans.kt
@@ -0,0 +1,2 @@
+package com.clean.filecleaner.ui.module
+
