f97aec5b4d1fddc3b2b3568c584e9ac639141955
KK <919578485@qq.com>
2024-12-31 11:20:55 +0800

    recyclerView动画

--- app/src/main/java/com/clean/filecleaner/ui/module/junk/adapter/JunkSearchAdapter.kt
+++ app/src/main/java/com/clean/filecleaner/ui/module/junk/adapter/JunkSearchAdapter.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.clean.filecleaner.R
 import com.clean.filecleaner.databinding.ItemJunkSearchBinding
+import com.clean.filecleaner.ext.startRotatingWithRotateAnimation
 import com.clean.filecleaner.ui.module.junk.bean.JunkSearchItem
 
 class JunkSearchAdapter(private val activity: Activity, val list: List<JunkSearchItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -30,8 +32,8 @@ class JunkSearchAdapter(private val activity: Activity, val list: List<JunkSearc
 //                size.text = formatFileSize(activity, item.size)
 //
 //                if (item.isLoading) {
-//                    loading.setImageResource(R.drawable.more_arrow)
-//                    loading.startRotatingWithRotateAnimation()
+                loading.setImageResource(R.drawable.more_arrow)
+                loading.startRotatingWithRotateAnimation(500)
 //                } else {
 //                    loading.stopRotatingWithRotateAnimation()
 //                    // loading.rotation = 0f
