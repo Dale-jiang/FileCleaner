b0a9f06d50b29232f1e338601e7f3431d41fd0d0
KK <919578485@qq.com>
2025-02-07 17:41:36 +0800

    recent

--- app/src/main/java/com/clean/filecleaner/ui/module/clean/recent/AppRecentInfoActivity.kt
+++ app/src/main/java/com/clean/filecleaner/ui/module/clean/recent/AppRecentInfoActivity.kt
@@ -1,19 +1,97 @@
 package com.clean.filecleaner.ui.module.clean.recent
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.addCallback
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.transition.TransitionManager
+import com.clean.filecleaner.R
 import com.clean.filecleaner.databinding.ActivityAppRecentInfoBinding
-import com.clean.filecleaner.databinding.ActivityAppRecentInfoPreBinding
-import com.clean.filecleaner.ext.hasUsagePermissions
 import com.clean.filecleaner.ext.immersiveMode
-import com.clean.filecleaner.ui.base.UsagePermissionBaseActivity
+import com.clean.filecleaner.ext.startRotatingWithRotateAnimation
+import com.clean.filecleaner.ext.stopRotatingWithRotateAnimation
+import com.clean.filecleaner.report.reporter.DataReportingUtils
+import com.clean.filecleaner.ui.ad.adManagerState
+import com.clean.filecleaner.ui.ad.canShow
+import com.clean.filecleaner.ui.ad.hasReachedUnusualAdLimit
+import com.clean.filecleaner.ui.ad.loadAd
+import com.clean.filecleaner.ui.ad.showFullScreenAd
+import com.clean.filecleaner.ui.base.BaseActivity
+import com.clean.filecleaner.ui.module.MainActivity
+import com.clean.filecleaner.ui.module.filemanager.allFilesContainerList
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class AppRecentInfoActivity : UsagePermissionBaseActivity<ActivityAppRecentInfoBinding>() {
+class AppRecentInfoActivity : BaseActivity<ActivityAppRecentInfoBinding>() {
     override fun setupImmersiveMode() = immersiveMode(binding.root)
     override fun inflateViewBinding(): ActivityAppRecentInfoBinding = ActivityAppRecentInfoBinding.inflate(layoutInflater)
 
-    override fun initView(savedInstanceState: Bundle?) {
+    private var timeTag = 0L
 
+    private fun setListeners() {
+        onBackPressedDispatcher.addCallback {
+            kotlin.runCatching {
+                allFilesContainerList.clear()
+            }
+            startActivity(Intent(this@AppRecentInfoActivity, MainActivity::class.java))
+            finish()
+        }
+
+        binding.loadingView.setOnClickListener { }
+
+        binding.toolbar.ivLeft.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        setListeners()
+
+        with(binding) {
+            toolbar.title.text = getString(R.string.apk)
+            ivLoading.startRotatingWithRotateAnimation()
+            showLoadingAnimation(preStr = getString(R.string.scanning)) {
+                tvLoading.text = it
+            }
+        }
+
+        lifecycleScope.launch {
+            delay(2000L)
+            stopLoadingAnim()
+            TransitionManager.beginDelayedTransition(binding.root)
+            binding.loadingView.isVisible = false
+        }
+    }
+
+
+    override fun stopLoadingAnim() {
+        super.stopLoadingAnim()
+        binding.ivLoading.stopRotatingWithRotateAnimation()
+    }
+
+    private fun fullScreenAdShow(onComplete: () -> Unit) = runCatching {
+        if (adManagerState.hasReachedUnusualAdLimit()) return@runCatching onComplete()
+        DataReportingUtils.postCustomEvent("fc_ad_chance", hashMapOf("ad_pos_id" to "fc_scan_int"))
+        val adState = adManagerState.fcBackScanIntState
+        if (!adState.canShow()) {
+            adState.loadAd(this)
+            return@runCatching onComplete()
+        }
+        lifecycleScope.launch {
+            while (!lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) {
+                delay(210L)
+            }
+            adState.showFullScreenAd(this@AppRecentInfoActivity, "fc_scan_int") { onComplete() }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLoadingAnim()
+    }
+
 }
 
 
